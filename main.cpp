da83564c0aab0c071ea60884aea880ad3cecd31d
Albert Yu <92063396+albertyu03@users.noreply.github.com>
2022-03-11 09:16:04 +0000

    cleaning up code and adding runtime analysis

--- main.cpp
+++ main.cpp
@@ -16,10 +16,6 @@
 using namespace std;
 
 
-
-
-bool parseLine(string &line, string &movieName, double &movieRating);
-
 int main(int argc, char** argv){
   
   if(argc < 2){
@@ -36,18 +32,11 @@ int main(int argc, char** argv){
     cerr << "Could not open file " << argv[1];
     exit(1);
   }
-  
-//Create an object of a STL data-structure to store all the movies
 vector<string> movNR;
 
   
 string line;
-// Read each file and store the name and rating
 while (getline (movieFile, line)){
-  // Use std::string movieName and double movieRating
-  // to construct your Movie objects
-  // cout << movieName << " has rating " << movieRating << endl;
-  // insert elements into your data structure
   if(line.at(0) == '"') {
     line = truncQuotes(line);
   }
@@ -101,42 +90,18 @@ for(int i = 2; i < argc; i++) {
   }
 }
 return 0;
-//  For each prefix,
-//  Find all movies that have that prefix and store them in an appropriate data structure
-//  If no movie with that prefix exists print the following message
-cout<<"No movies found with prefix "<<"<replace with prefix>"<<endl<<endl;
-
-//  For each prefix,
-//  Print the highest rated movie with that prefix if it exists.
-cout << "Best movie with prefix "<<"<replace with prefix>"<<" is: " << "replace with moview name" <<" with rating " << std::fixed << std::setprecision(1) << "replace with movie rating"<< endl;
-    
-
-
-return 0;
 }
 
 /* Add your run time analysis for part 3 of the assignment here as commented block*/
-
-
 /*
-bool parseLine(string &line, string &movieName, double &movieRating) {
-  if(line.length() <= 0) return false;
-  string tempRating = "";
+FINDING RUNTIME:
+As per instructions, we assume that our movies and prefix lists have already been stored.
+Our given variables are m(prefix#), k(most matching), and n(movies#)
 
-  bool flag = false;
-  movieName = tempRating = "", movieRating = 0.0, flag = false;
-
-  for (int i = 0; i < line.length(); i++){
-    if(flag) tempRating += line[i];
-    else if(line[i]==','&& line[0]!='"') flag = true;
-    else {
-		  if(i==0 && line[0]=='"') continue;
-		  if(line[i]=='"'){ i++; flag=true; continue;}
-		  movieName += line[i];
-	  }
-  }
-  
-  movieRating = stod(tempRating);
-  return true;
-}
+1. First, we know that for every prefix we are iterating through n values. 
+2. While iterating through n values, we are pushing k of them to a priority queue, which has a time complexity of o(log k).
+3. As such, one "loop" of forming a priority queue of all movies matching that prefix has a time complexity of O(n + k[log(k)])
+4. Of course, this will be for m prefixes, putting our complexity so far at O(m * [n + k(log[k])])
+5. We are fetching, pushing, and eventually printing m prefixes. However, this has no significant impact on the runtime. Simplifying would yield O(m * [n + k(log[k]) + 3]), which can be pretty much disregarded on large scale.
+6. Simplifying our complexity for #4 yields O(mn + mk[log(k)])
 */
