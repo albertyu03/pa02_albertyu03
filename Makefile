a1ae61b3804f2137937cab812ff55bc4b26ca262
Albert Yu <albertyu@cs.ucsb.edu>
2022-03-10 23:29:11 -0800

    minor changes and Makefile creation

---
+++ Makefile
@@ -0,0 +1,12 @@
+CXX_FLAG = --std=c++11 -g
+
+all: runMovies
+runMovies: main.o movies.o
+	g++ $(CXX_FLAG) -o runMovies main.o movies.o
+main.o: main.cpp
+	g++ -c $(CXX_FLAG) main.cpp
+movies.o: movies.cpp
+	g++ -c $(CXX_FLAG) movies.cpp
+
+clean:
+	rm -f main main *.o runMovies
--- movies.h
+++ movies.h
@@ -9,7 +9,7 @@ using namespace std;
 class cmp {
   public:
     double retRating(string cMovie) const;
-    bool cmp::operator() (string& rate1, string& rate2) const {
+    bool operator() (string& rate1, string& rate2) const {
       return(retRating(rate1)<retRating(rate2));
     }
 };
