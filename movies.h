07368c4b59a98e7b5b35b07b647fdb186b69b858
Albert Yu <92063396+albertyu03@users.noreply.github.com>
2022-03-11 08:18:00 +0000

    adding comparison for equal ratings

--- movies.h
+++ movies.h
@@ -11,8 +11,12 @@ string truncName(string cMovie);
 class cmp {
   public:
     bool operator() (string& rate1, string& rate2) const {
+      if(retRating(rate1) == retRating(rate2)) {
+        return (truncName(rate1)<truncName(rate2));
+      }
       return(retRating(rate1)<retRating(rate2));
     }
 };
+
 
 #endif
